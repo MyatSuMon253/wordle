c2653f2c799824c91f2fe9dd85f22aca884439c3
MyatSuMon253 <myatsumon2531999@gmail.com>
2023-12-16 23:52:18 +0630

    feat: game logic

--- src/components/Game/GuessResult.js
+++ src/components/Game/GuessResult.js
@@ -2,11 +2,11 @@ import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import { range } from "../../utils";
 import Guess from "./Guess";
 
-const GuessResult = ({ guesses }) => {
+const GuessResult = ({ guesses, answer }) => {
   return (
     <div className="guess-guesses">
-      {range(NUM_OF_GUESSES_ALLOWED).map(({ num }) => (
-        <Guess key={num} value={guesses[num]} />
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} value={guesses[num]} answer={answer} />
       ))}
     </div>
   );
